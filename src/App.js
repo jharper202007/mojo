08152558d72ca12768271553c270b5ac6b65021b
james-harper <jameswilliamharper@gmail.com>
2020-08-02 20:09:31 +0100

    Remove unused files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import NavBar from './components/layout/NavBar';
+import BuildingsPage from './components/pages/Buildings';
+import GenericPage from './components/pages/Generic';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          {/* will match either '/' or '/buildings' */}
+          <Route path="/(buildings)?" exact>
+            <BuildingsPage />
+          </Route>
+          {/* fallback route to catch anything else */}
+          <Route path="/">
+            <GenericPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
