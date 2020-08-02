e9bc8d2354c30b2bd647e4bf760bdedb5ffe1930
james-harper <jameswilliamharper@gmail.com>
2020-08-02 22:11:49 +0100

    Move main content to right of side nav

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,25 @@ import {
 import NavBar from './components/layout/NavBar';
 import BuildingsPage from './components/pages/Buildings';
 import GenericPage from './components/pages/Generic';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
-        <Switch>
-          {/* will match either '/' or '/buildings' */}
-          <Route path="/(buildings)?" exact>
-            <BuildingsPage />
-          </Route>
-          {/* fallback route to catch anything else */}
-          <Route path="/">
-            <GenericPage />
-          </Route>
-        </Switch>
+        <div className='main'>
+          <Switch>
+            {/* will match either '/' or '/buildings' */}
+            <Route path="/(buildings)?" exact>
+              <BuildingsPage />
+            </Route>
+            {/* fallback route to catch anything else */}
+            <Route path="/">
+              <GenericPage />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
