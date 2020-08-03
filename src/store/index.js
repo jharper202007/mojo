5ccbcc2335d0d3dad14d07e1f73da37459972c04
james-harper <jameswilliamharper@gmail.com>
2020-08-03 13:40:50 +0100

    Fix bug related to redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import rootReducer from '../reducers';
 
+const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose);
+
 export default createStore(
   rootReducer,
-  compose(applyMiddleware(thunk), composeWithDevTools()),
+  composeEnhancers(applyMiddleware(thunk)),
 );
